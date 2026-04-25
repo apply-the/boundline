88d0331bdc8b2c60e5f7e2ddb9462b97e2b0946e
Roberto Trunfio <roberto.trunfio@gmail.com>
2026-04-25 17:42:43 +0200

    chore: improved version to 0.5.0

--- tests/integration/session_cli_flow.rs
+++ tests/integration/session_cli_flow.rs
@@ -14,7 +14,7 @@ fn temp_workspace() -> PathBuf {
     fs::create_dir_all(&workspace).unwrap();
     fs::write(
         workspace.join("Cargo.toml"),
-        "[package]\nname = \"synod-fixture\"\nversion = \"0.4.0\"\nedition = \"2024\"\n",
+        "[package]\nname = \"synod-fixture\"\nversion = \"0.5.0\"\nedition = \"2024\"\n",
     )
     .unwrap();
     workspace
